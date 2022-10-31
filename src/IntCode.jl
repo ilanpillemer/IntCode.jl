13d45f7061170d35ad53a8fdc1a5a447f1521cce
Ilan <ilan.pillemer@gmail.com>
2022-10-31 17:01:42 +0000

    opcodes 5,6,7,8

--- src/IntCode.jl
+++ src/IntCode.jl
@@ -25,6 +25,30 @@ function mul(p, x, y, px, py)
     a * b
 end
 
+function less(p, x, y, px, py)
+    a = px ? p[p[x]] : p[x]
+    b = py ? p[p[y]] : p[y]
+    a < b ? 1 : 0
+end
+
+function eql(p, x, y, px, py)
+    a = px ? p[p[x]] : p[x]
+    b = py ? p[p[y]] : p[y]
+    a == b ? 1 : 0
+end
+
+function jnz(pc, p, x, y, px, py)
+    a = px ? p[p[x]] : p[x]
+    b = py ? p[p[y]] : p[y]
+    a > 0 ? b : pc + 3
+end
+
+function jz(pc, p, x, y, px, py)
+    a = px ? p[p[x]] : p[x]
+    b = py ? p[p[y]] : p[y]
+    a == 0 ? b : pc + 3
+end
+
 function output(p, x, px)
     a = px ? p[p[x]] : p[x]
     println(a)
@@ -40,8 +64,9 @@ function exec(p)
     end
     p[0]
 end
-
-get_input() = 1
+n = 1
+set_input(x) = global n = x
+get_input() = n
 
 function get_modes(opcode::Int64)
     s = "$opcode"
@@ -68,6 +93,20 @@ function op(pc, p, arg, opcode::Int64)
     elseif opcode == 4
         output(p, arg[1], x)
         return pc + 2
+    elseif opcode == 5 # jump-if-true
+        pc = jnz(pc, p, arg[1], arg[2], x, y)
+        return pc
+    elseif opcode == 6 # jump-if-false
+        pc = jz(pc, p, arg[1], arg[2], x, y)
+        return pc
+    elseif opcode == 7
+        a = p[arg[3]]
+        p[a] = less(p, arg[1], arg[2], x, y)
+        return pc + 4
+    elseif opcode == 8
+        a = p[arg[3]]
+        p[a] = eql(p, arg[1], arg[2], x, y)
+        return pc + 4
     else
         println("panic: unknown opcode $opcode")
         exit()
