962a4c9413cc704be0f0741fb6f669d6f2aedcd6
Ilan <ilan.pillemer@gmail.com>
2022-10-31 16:11:12 +0000

    opcodes 3,4 and modes

--- src/IntCode.jl
+++ src/IntCode.jl
@@ -13,35 +13,65 @@ function load(name)
     p
 end
 
-function add(p, x, y)
-    a = p[x]
-    b = p[y]
-    p[a] + p[b]
+function add(p, x, y, px, py)
+    a = px ? p[p[x]] : p[x]
+    b = py ? p[p[y]] : p[y]
+    a + b
 end
 
-function mul(p, x, y)
-    a = p[x]
-    b = p[y]
-    p[a] * p[b]
+function mul(p, x, y, px, py)
+    a = px ? p[p[x]] : p[x]
+    b = py ? p[p[y]] : p[y]
+    a * b
+end
+
+function output(p, x, px)
+    a = px ? p[p[x]] : p[x]
+    println(a)
 end
 
 function exec(p)
     pc = 0
     opcode = p[pc]
     while opcode != 99
-        x = pc + 1
-        y = pc + 2
-        z = pc + 3
-        a = p[z]
-        if opcode == 1
-            p[a] = add(p, x, y)
-        elseif opcode == 2
-            p[a] = mul(p, x, y)
-        end
-        pc = pc + 4
+        args = [pc + 1, pc + 2, pc + 3]
+        pc = pc + op(p, args, opcode)
         opcode = p[pc]
     end
     p[0]
+end
+
+get_input() = 1
+
+function get_modes(opcode::Int64)
+    s = "$opcode"
+    s = lpad(s, 5, "0")
+    #println(s)
+    (s[3] == '0', s[2] == '0', s[3] == '0')
+end
+
+function op(p, arg, opcode::Int64)
+    (x, y, z) = get_modes(opcode)
+    opcode = opcode % 100
+    if opcode == 1
+        a = p[arg[3]]
+        p[a] = add(p, arg[1], arg[2], x, y)
+        return 4
+    elseif opcode == 2
+        a = p[arg[3]]
+        p[a] = mul(p, arg[1], arg[2], x, y)
+        return 4
+    elseif opcode == 3
+        a = p[arg[1]]
+        p[a] = get_input()
+        return 2
+    elseif opcode == 4
+        output(p, arg[1], x)
+        return 2
+    else
+        println("panic: unknown opcode $opcode")
+        exit()
+    end
 end
 
 
